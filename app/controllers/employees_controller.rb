6e944c2a879f1a450368c78390f0bbcdfbe16437
Michael Daross <daross@teamshares.com>
2024-01-30 17:13:54 -0600

    Working keystroke search
    Thanks Kat!

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,77 +1,14 @@
 class EmployeesController < ApplicationController
-  before_action :set_employee, only: %i[show edit update destroy]
-
-  # GET /employees or /employees.json
   def index
-    search = params[:search]
-    @employees = if search.present?
-                   Employee.where('first_name ILIKE :search OR last_name ILIKE :search OR email ILIKE :search', search: "%#{search}%")
+    query = params[:query]
+    @employees = if query.present?
+                   Employee.where('first_name ILIKE :query OR last_name ILIKE :query OR email ILIKE :query', query: "%#{query}%")
                  else
                    Employee.all
                  end
   end
 
-  # GET /employees/1 or /employees/1.json
-  def show; end
-
-  # GET /employees/new
-  def new
-    @employee = Employee.new
-  end
-
-  # GET /employees/1/edit
-  def edit; end
-
-  def search; end
-
-  # POST /employees or /employees.json
-  def create
-    @employee = Employee.new(employee_params)
-
-    respond_to do |format|
-      if @employee.save
-        format.html { redirect_to employee_url(@employee), notice: 'Employee was successfully created.' }
-        format.json { render :show, status: :created, location: @employee }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @employee.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /employees/1 or /employees/1.json
-  def update
-    respond_to do |format|
-      if @employee.update(employee_params)
-        format.html { redirect_to employee_url(@employee), notice: 'Employee was successfully updated.' }
-        format.json { render :show, status: :ok, location: @employee }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @employee.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /employees/1 or /employees/1.json
-  def destroy
-    @employee.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to employees_url, notice: 'Employee was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_employee
+  def show
     @employee = Employee.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def employee_params
-    params.require(:employee).permit(:search, :first_name, :last_name, :phone, :email, :birthday, :country, :active,
-                                     :company_id)
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,7 +7,7 @@ pin "@floating-ui/dom", to: "@floating-ui--dom.js" # @1.6.0
 pin "@floating-ui/utils", to: "@floating-ui--utils.js" # @0.2.1
 pin "@floating-ui/utils/dom", to: "@floating-ui--utils--dom.js" # @0.2.1
 pin "@lit/reactive-element", to: "@lit--reactive-element.js" # @2.0.3
-pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.0
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.0
 pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @7.1.3
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
