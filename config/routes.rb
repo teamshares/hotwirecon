cc8fcf16366baa1a4c5a92b4ed523d907185a9ab
Michael Daross <daross@teamshares.com>
2024-01-30 14:50:47 -0600

    Updating for Kat

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  root "employees#index"
-
   resources :employees
-  resources :companies
+  root to: redirect("/employees")
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
