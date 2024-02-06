15f35cdbdf361164943bb3d06e53c2340f91c47a
Dave Sims <davsims@gmail.com>
2024-02-06 13:44:57 -0600

    add edit and update actions to controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -11,4 +11,24 @@ class EmployeesController < ApplicationController
   def show
     @employee = Employee.find(params[:id])
   end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+
+    if @employee.update(employee_params)
+      redirect_to @employee
+    else
+      render :edit
+    end
+  end
+end
+
+private 
+
+def employee_params
+  params.require(:employee).permit(:first_name, :last_name, :email, :drivers_license)
 end
