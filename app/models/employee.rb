1415e13ac9f45571dcfec0da5844fbff4ae10c47
Dave Sims <davsims@gmail.com>
2024-02-07 17:26:54 -0600

    add controller actions; form submit stream

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,4 +4,8 @@ class Employee < ApplicationRecord
   after_update_commit -> {
     broadcast_replace_to "employees", locals: { employee: self }
   }
+
+  after_create_commit -> {
+    broadcast_append_to "employees"
+  }
 end
