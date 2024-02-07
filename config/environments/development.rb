014c030a8b79cd0c46d2167c04bacdc287ab2f47
Michael Daross <daross@teamshares.com>
2024-02-07 16:34:31 -0600

    Minor style fixes and adding template inspector

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,8 +49,32 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
+  # Options for the Rails Template Inpsector tool loaded in application.html.slim. You can override
+  # these locally via environment variables.
+  # -----------
+  # @url_prefix: The string that launches your code editor from the browser. Options:
+  # - Visual Studio Code: vscode://file
+  # - RubyMine: x-mine://open?file=
+  # - MacVIM: mvim://open?url=file://
+  # - Emacs: emacs://open?url=file://
+  # - IntelliJ: idea://open?file=
+  # - PyCharm: pycharm://open?file=
+  # - SublimeText: ??? Maybe try subl://file
+  #
+  #   Example: To use RubyMine, add the following line in .bashrc:
+  #     export TEMPLATE_INSPECTOR_URL_PREFIX="x-mine://open?file="
+  # -----------
+  # @combo_key: The keyboard shortcut to launch the inspector
+  # -----------
+  # @no_auto_disable: Whether to automatically close the tool after taking you to your code editor
+  config.template_inspector_options = {
+    url_prefix: ENV["TEMPLATE_INSPECTOR_URL_PREFIX"] || "vscode://file",
+    combo_key: ENV["TEMPLATE_INSPECTOR_COMBO_KEY"] || "command-shift-v",
+    no_auto_disable: ENV["TEMPLATE_INSPECTOR_NO_AUTO_DISABLE"] || false,
+  }
+
   # Annotate rendered view with file names.
-  # config.action_view.annotate_rendered_view_with_filenames = true
+  config.action_view.annotate_rendered_view_with_filenames = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
